cd69f062b906bdaf265136fb9246bef9c7bec711
Dallas <dallas.mcgroarty@gmail.com>
2023-11-24 17:37:37 -0800

    initial books app done

--- books/src/App.js
+++ books/src/App.js
@@ -1,8 +1,29 @@
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
     const [books, setBooks] = useState([]);
+
+    const deleteBookById = (id) => {
+        const updatedbooks = books.filter((book) => {
+            return book.id !== id;
+        })
+
+        setBooks(updatedbooks);
+    };
+
+    const editBookById = (id, title) => {
+        const updatedbooks = books.map((book) => {
+            if (book.id == id) {
+                return {...book, title}
+            }
+
+            return book;
+        })
+
+        setBooks(updatedbooks);
+    };
 
     const createBook = (title) => {
         const updatedbooks = [...books, 
@@ -16,7 +37,9 @@ function App() {
     }
 
     return (
-        <div>
+        <div className='app'>
+            <h1>Reading List</h1>
+            <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
             <BookCreate onCreate={createBook} />
         </div>
     );
--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,5 +1,16 @@
-function BookList() {
-    return <div>book list</div>
+import BookShow from './BookShow';
+
+function BookList({ books, onDelete, onEdit }) {
+
+    const renderedBooks = books.map((book) => {
+        return <BookShow key={book.id} book={book} onDelete={onDelete} onEdit={onEdit} />;
+    });
+
+    return (
+        <div className='book-list'>
+            {renderedBooks}
+        </div>
+    )
 }
 
 export default BookList;
