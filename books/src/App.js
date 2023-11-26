badb6a755b087c770ab5cebe4ff8a7f96dca01a2
Dallas <dallas.mcgroarty@gmail.com>
2023-11-26 15:16:51 -0800

    incorporated api to handle data persistence

--- books/src/App.js
+++ books/src/App.js
@@ -1,11 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
     const [books, setBooks] = useState([]);
 
-    const deleteBookById = (id) => {
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    }
+
+    useEffect(() => {
+        fetchBooks();
+    }, []);
+
+    // don't do this
+    // call fetchbooks();
+
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+
         const updatedbooks = books.filter((book) => {
             return book.id !== id;
         })
@@ -13,10 +29,14 @@ function App() {
         setBooks(updatedbooks);
     };
 
-    const editBookById = (id, title) => {
+    const editBookById = async (id, title) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title
+        });
+
         const updatedbooks = books.map((book) => {
             if (book.id === id) {
-                return {...book, title}
+                return {...book, ...response.data}
             }
 
             return book;
@@ -25,15 +45,21 @@ function App() {
         setBooks(updatedbooks);
     };
 
-    const createBook = (title) => {
-        const updatedbooks = [...books, 
-            {
-                id: Math.round(Math.random() * 9999), 
-                title
-            }
-        ];
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title
+        });
 
-        setBooks(updatedbooks);
+        setBooks([...books, response.data]);
+
+        // const updatedbooks = [...books, 
+        //     {
+        //         id: Math.round(Math.random() * 9999), 
+        //         title
+        //     }
+        // ];
+
+        // setBooks(updatedbooks);
     }
 
     return (
