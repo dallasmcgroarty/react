e864c621de5c03ef119657fce93dc049a6cdc796
Dallas <dallas.mcgroarty@gmail.com>
2023-11-28 17:23:46 -0800

    more on useEffect and useCallback

--- books/src/App.js
+++ books/src/App.js
@@ -1,14 +1,14 @@
 import { useEffect, useContext } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
-import BooksContext, { Provider } from './context/books';
+import BooksContext from './context/books';
 
 function App() {
     const { fetchBooks } = useContext(BooksContext)
 
     useEffect(() => {
         fetchBooks();
-    }, []);
+    }, [fetchBooks]);
 
     return (
         <div className='app'>
