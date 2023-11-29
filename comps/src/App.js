8eaa198d165a449e7c3119bde6dbed5727b7c845
Dallas <dallas.mcgroarty@gmail.com>
2023-11-29 15:17:14 -0800

    resuable button component done

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,22 +1,36 @@
 import Button from './Button';
+import { GoBell, GoCloud, GoDatabase } from 'react-icons/go';
 
 function App() {
+    const handleClick = () => {
+        console.log('click');
+    }
+
     return(
         <div>
             <div>
-                <Button success rounded outline>Text here</Button>
+                <Button success rounded outline className='mb-5' onClick={handleClick}>
+                    <GoBell /> 
+                    Text here
+                </Button>
             </div>
             <div>
-                <Button danger outline>Buy now</Button>
+                <Button danger outline onMouseEnter={handleClick}>
+                    <GoCloud />
+                    Buy now
+                </Button>
             </div>
             <div>
-                <Button warning>Click me</Button>
+                <Button warning onMouseLeave={handleClick}>
+                    <GoDatabase />
+                    Click me
+                </Button>
             </div>
             <div>
                 <Button secondary outline>Show more</Button>
             </div>
             <div>
-                <Button seconday rounded>Something</Button>
+                <Button primary rounded>Something</Button>
             </div>
         </div>
     )
