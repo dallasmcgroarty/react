2b5d32d800efcc83560ae2d87a79219921ce1180
Dallas <dallas.mcgroarty@gmail.com>
2024-01-22 20:00:35 -0800

    more react

--- media/src/store/thunks/fetchUsers.js
+++ media/src/store/thunks/fetchUsers.js
@@ -3,7 +3,18 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
     const response = await axios.get('http://localhost:3005/users');
+
+    // dev only!
+    await pause(2000);
+
     return response.data;
 });
+
+// dev only!
+const pause = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, duration);
+    })
+};
 
 export { fetchUsers };
