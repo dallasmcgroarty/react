795756d3b49ed3dc7bf89b44a4493ea4247d0f8c
Dallas <dallas.mcgroarty@gmail.com>
2024-02-03 10:16:55 -0800

    react course complete

--- media/src/components/PhotosList.js
+++ media/src/components/PhotosList.js
@@ -30,7 +30,7 @@ function PhotosList({ album }) {
         <h3 className="text-lg font-bold">Photos In {album.title}</h3>
         <Button loading={addPhotoResults.isLoading} onClick={handleAddPhoto}>+ Add Photo</Button>
       </div>
-      <div>
+      <div className="mx-8 flex flex-row flex-wrap justify-center">
         {content}
       </div>
     </div>
